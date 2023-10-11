ffb4a0a38d52da5815b12d30e3599b25e374ac33
zchaplin <zchaplin@ucsc.edu>
2023-10-11 14:50:21 -0700

    Step One Create Button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+app.innerHTML = `
+<div>
+  <div class="card">
+    <button id="counter" type="button"></button>
+  </div>
+</div>
+`;
 const gameName = "Zane's gamer";
 
 document.title = gameName;
+const element2 = document.querySelector<HTMLButtonElement>("#counter")!;
+
+element2.innerHTML = '💨';
+
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
