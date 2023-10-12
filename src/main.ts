5c616d2b4a332923f4209f352187bb052d44ef18
zchaplin <zchaplin@ucsc.edu>
2023-10-12 02:37:34 -0700

    Step 10 Done

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ app.innerHTML = `
     <button id="upgradeOne" type="button"></button>
     <button id="upgradeTwo" type="button"></button>
     <button id="upgradeThree" type="button"></button>
+    <button id="upgradeFour" type="button"></button>
+    <button id="upgradeFive" type="button"></button>
     <button id="perSec" type="button"></button>
   </div>
 </div>
@@ -24,32 +26,61 @@ const tottal = document.querySelector<HTMLButtonElement>("#tottal")!;
 interface Upgrade {
   button: HTMLButtonElement;
   name: string;
+  description: string;
   cost: number;
   effect: number;
   times: number;
+  type: string;
 }
 
 const upgrades: { [key: string]: Upgrade } = {
   upgradeOne: {
     button: document.querySelector<HTMLButtonElement>("#upgradeOne")!,
     name: "Chicken",
+    description: "This will make you fart .1 seconds faster",
     cost: 10,
     effect: 0.1,
     times: 0,
+    type: "normal",
   },
   upgradeTwo: {
     button: document.querySelector<HTMLButtonElement>("#upgradeTwo")!,
     name: "Beans",
+    description:
+      "The magical fruit increases flatulence production by 2 a second",
     cost: 100,
     effect: 2,
     times: 0,
+    type: "normal",
   },
   upgradeThree: {
     button: document.querySelector<HTMLButtonElement>("#upgradeThree")!,
     name: "Todd Cobell",
+    description:
+      "The holly Mr.Cobell is said to have the magic combo to sckyrocket you to mars",
     cost: 1000,
     effect: 50,
     times: 0,
+    type: "normal",
+  },
+  upgradeFour: {
+    button: document.querySelector<HTMLButtonElement>("#upgradeFour")!,
+    name: "Stronger Gut",
+    description:
+      "Increase your ability to consume food at the cost of less speed",
+    cost: 150,
+    effect: 0.9,
+    times: 0,
+    type: "boost",
+  },
+  upgradeFive: {
+    button: document.querySelector<HTMLButtonElement>("#upgradeFive")!,
+    name: "The Nuclear Option",
+    description: "There are 19 billion kilocalories in a kg of uranium",
+    cost: 100000,
+    effect: 10000,
+    times: 0,
+    type: "normal",
   },
 };
 
@@ -69,21 +100,38 @@ const setCounter = (count: number) => {
   perSec.innerHTML = `You speed up ${updateFrameSpeed.toFixed(
     1,
   )} mph per second`;
-  tottal.innerHTML = `You are going ${counter} miles per hour`;
+  tottal.innerHTML = `You are going ${counter.toFixed(2)} miles per hour`;
   requestAnimationFrame(() => setCounter(counter));
 };
 
 function upgradeFunc(upgrade: string) {
+  if (upgrades[upgrade].type === "boost") {
+    console.log("Boost");
+    if (counter > upgrades[upgrade].cost) {
+      counter -= upgrades[upgrade].cost;
+      upgrades[upgrade].cost *= 1.3;
+      for (const grade in upgrades) {
+        upgrades[grade].cost *= upgrades[upgrade].effect;
+      }
+      drawUpgrades();
+    }
+    return;
+  }
   if (counter >= upgrades[upgrade].cost) {
     counter -= upgrades[upgrade].cost;
     updateFrameSpeed += upgrades[upgrade].effect;
     upgrades[upgrade].cost *= 1.15;
     upgrades[upgrade].times += 1;
+    drawUpgrades();
+  }
+}
+function drawUpgrades() {
+  for (const upgrade in upgrades) {
     upgrades[upgrade].button.innerHTML = `${
       upgrades[upgrade].name
-    }<div>Cost: ${upgrades[upgrade].cost.toFixed(2)} Speed<div>Count: ${
-      upgrades[upgrade].times
-    }`;
+    }<div>Cost: ${upgrades[upgrade].cost.toFixed(2)} Speed<div>Descripton: ${
+      upgrades[upgrade].description
+    } <div>Count: ${upgrades[upgrade].times}`;
   }
 }
 setCounter(0);
@@ -94,11 +142,7 @@ for (const upgrade in upgrades) {
   upgrades[upgrade].button.addEventListener("click", () =>
     upgradeFunc(upgrade),
   );
-  upgrades[upgrade].button.innerHTML = `${
-    upgrades[upgrade].name
-  }<div>Cost: ${upgrades[upgrade].cost.toFixed(2)} Speed<div>Count: ${
-    upgrades[upgrade].times
-  }`;
+  drawUpgrades();
 }
 
 const header = document.createElement("h1");
