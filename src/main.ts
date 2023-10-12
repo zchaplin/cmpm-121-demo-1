f3431a15e5171a998cfe4f69052537d86b001d96
zchaplin <zchaplin@ucsc.edu>
2023-10-12 00:47:31 -0700

    Step 4 done

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,25 @@ const gameName = "Zane's gamer";
 let counter = 0;
 document.title = gameName;
 const element2 = document.querySelector<HTMLButtonElement>("#counter")!;
+let lastTime = performance.now();
+
 const setCounter = (count: number) => {
+  const currentTime = performance.now();
+  const deltaTime = currentTime - lastTime;
+  const updateFrameSpeed = 1;
+  console.log(count);
   counter = count;
+  if (deltaTime > 1000 / updateFrameSpeed) {
+    lastTime = currentTime;
+    counter += 1;
+  }
   element2.innerHTML = `You have created ${counter} polutions💨`;
   console.log("Wasa");
+  requestAnimationFrame(() => setCounter(counter));
 };
-setInterval(() => setCounter(counter + 1), 1000);
-
-element2.addEventListener("click", () => setCounter(counter + 1));
 setCounter(0);
+element2.addEventListener("click", () => setCounter(counter + 1));
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
